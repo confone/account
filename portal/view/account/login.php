69f435e53f51a950663f6409241ad9b92233d350
peng.shen <peng.shen@indochino.com>
2014-04-14 11:29:36 -0700

    login css

--- portal/view/account/login.php
+++ portal/view/account/login.php
@@ -5,18 +5,23 @@ $stylesheets = array('account.css');
 include 'view/include/header.php';
 ?>
 <script type="text/javascript">var RecaptchaOptions = { theme : 'clean' };</script>
+<div id="login">
 <form action="/login" method="post" enctype="multipart/form-data" accept-charset="UTF-8">
-<table>
-<tr><td>Email: </td><td><input class="text_field round4" type="text" name="username" value="" /></td></tr>
-<tr><td>Password: </td><td><input class="text_field round4" type="password" name="password" value="" /></td></tr>
-</table>
+<label class="field_label">Email:</label>
+<div class="field_div"><input class="text_field round4" type="text" name="username" value="" /></div>
+<label class="field_label">Password:</label>
+<div class="field_div"><input class="text_field round4" type="password" name="password" value="" /></div>
 <?php if ($recaptcha) {
     global $recaptcha_public_key;
     echo recaptcha_get_html($recaptcha_public_key, 'Invalid ReCAPTCHA', true);
 } ?>
 <input type="hidden" name="redirect_uri" value="<?=$redirect_uri ?>" />
-<input class="button round4" type="submit" value="Sign In" />
+<div id="rem_sub">
+<input type="checkbox" name="keep_login" id="keep_login" /><label for="keep_login" class="keep_login">Keep me logged in</label>
+<input class="button round4 right" type="submit" value="Sign In" />
+</div>
 </form>
+</div>
 <?php 
 include 'view/include/footer.php'
 ?>
